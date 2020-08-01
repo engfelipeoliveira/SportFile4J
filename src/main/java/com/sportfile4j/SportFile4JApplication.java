60687a697ff4bea46e55eadb972da0accfda1a6c
ads14331 <ads14331@PPF8026.ssq.local>
2020-07-31 21:24:40 -0400

    first commit

---
+++ src/main/java/com/sportfile4j/SportFile4JApplication.java
@@ -0,0 +1,20 @@
+package com.sportfile4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportFile4JApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportFile4JApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
